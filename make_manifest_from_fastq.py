00fc1014ed4a62c1722e274f653a475cbf0180da
Xin-Zhan Liu <114778523+Xinzhan-Liu@users.noreply.github.com>
2023-01-24 13:46:37 -0800

    Update make_manifest_from_fastq.py

--- make_manifest_from_fastq.py
+++ make_manifest_from_fastq.py
@@ -1,16 +1,8 @@
 #!/bin/bash
 ls *.fastq.gz
-while read i;
-do echo "$i";
-zcat $i | grep -c '+';
-done
-
-
-
-
-for i in *.fastq.gz;
-do 
-zcat *.L001.fastq.gz | grep -c '+';
+for file in *.fastq.gz;
+do echo "$file";
+zcat "$file" | grep -c '+';
 done
 
 
